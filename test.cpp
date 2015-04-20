ba243becb1d6c4d820ac56d15d5334ea17ad30d8
Angel Yu <angelyu@mit.edu>
2015-04-20 00:01:28 -0400

    nth commit

--- test.cpp
+++ test.cpp
@@ -1,22 +1,41 @@
-#include <NTL/ZZ_pXFactoring.h>
+#include <NTL/mat_ZZ_p.h>
+#include <NTL/ZZ_p.h>
+#include <NTL/ZZ.h>
+#include <NTL/vec_ZZ_p.h>
+#include <NTL/vec_ZZ.h>
+
 
 using namespace std;
 using namespace NTL;
 
+const ZZ w = 123456789;
+
+vec_ZZ_p decrypt(mat_ZZ_p S, vec_ZZ_p c);
+
+// returns c*
+vec_ZZ_p getBitVector(vec_ZZ_p c);
+
+// returns S*
+mat_ZZ_p getBitMatrix(mat_ZZ_p S);
+
+// returns S
+mat_ZZ_p getSecretKey(mat_ZZ_p T);
+
+// returns M
+mat_ZZ_p keySwitchMatrix(mat_ZZ_p S, mat_ZZ_p T);
+
+// finds c* then returns Mc*
+vec_ZZ_p keySwitch(mat_ZZ_p M, vec_ZZ_p c);
+
+// as described, treating I as the secret key and wx as ciphertext
+vec_ZZ_p encrypt(mat_ZZ_p T, vec_ZZ_p x);
+
+
+mat_ZZ_p getRandomMatrix(int m, int n);
+
 int main()
 {
-   ZZ p;
-   cin >> p;
-   ZZ_p::init(p);
 
-   ZZ_pX f;
-   cin >> f;
-
-   Vec< Pair< ZZ_pX, long > > factors;
-
-   CanZass(factors, f);  // calls "Cantor/Zassenhaus" algorithm
-
-   cout << factors << "\n";
    return 0;
 }
 
