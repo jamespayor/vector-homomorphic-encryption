df23a095dd8df25f8429835cedbc44b2e7a17574
Angel Yu <angelyu@mit.edu>
2015-04-20 00:50:43 -0400

    implemented decrypt

--- test.cpp
+++ test.cpp
@@ -3,14 +3,15 @@
 #include <NTL/ZZ.h>
 #include <NTL/vec_ZZ_p.h>
 #include <NTL/vec_ZZ.h>
+#include <cmath>
 
 
 using namespace std;
 using namespace NTL;
 
-const ZZ w = 123456789;
+const ZZ w(12345), q((1LL << 31LL) - 1LL);
 
-vec_ZZ_p decrypt(mat_ZZ_p S, vec_ZZ_p c);
+vec_ZZ decrypt(mat_ZZ_p S, vec_ZZ_p c);
 
 // returns c*
 vec_ZZ_p getBitVector(vec_ZZ_p c);
@@ -33,9 +34,20 @@ vec_ZZ_p encrypt(mat_ZZ_p T, vec_ZZ_p x);
 
 mat_ZZ_p getRandomMatrix(int m, int n);
 
+
+vec_ZZ decrypt(mat_ZZ_p S, vec_ZZ_p c) {
+	vec_ZZ_p Sc = S*c;
+	vec_ZZ output;
+	output.SetLength(Sc.length());
+	for (unsigned int i=0; i<Sc.length(); i++) {
+		output[i] = (rep(Sc[i])+w/2)/w;
+	}
+	return output;
+}
+
 int main()
 {
-
+	ZZ_p::init(q);
    return 0;
 }
 
