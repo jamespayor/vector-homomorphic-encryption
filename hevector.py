bfa56bf9ee0e66d397652db0750a0c99c4437082
James Payor <james@payor.com.au>
2015-04-26 04:42:16 -0400

    Add initial python vector interface.

---
+++ hevector.py
@@ -0,0 +1,32 @@
+
+INNER_PRODUCT = '*'
+ADD = '+'
+
+def tupleToVec(t):
+	if type(t) is int: return str(t)
+	return '[%s]' % ' '.join(map(tupleToVec,t))
+
+def vecToTuple(v):
+	return tuple(map(int, v.strip('[]').split()))
+
+def send(ops):
+	return '\n'.join(v if type(v) is str else tupleToVec(v) for v in ops)
+
+def recv(output):
+	return tuple(vecToTuple(l) for l in map(str.strip, output.splitlines()))
+
+def evaluate(operations):
+	import subprocess
+	output, error = subprocess.Popen(['iven'], shell=True).communicate(send(operations))
+
+	if error:
+		from sys import stderr
+		stderr.write(error + '\n')
+		stderr.flush()
+
+	return recv(output)
+
+
+
+
+
