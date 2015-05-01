456a9304624b0e133118b378fa80b958958228d2
James Payor <james@payor.com.au>
2015-05-01 04:33:58 -0400

    Fixing.

--- hevector.py
+++ hevector.py
@@ -1,44 +1,45 @@
 
 def tupleToVec(t):
-	if type(t) is int: return str(t).strip('L')
-	return 'vector [%s]' % ' '.join(map(tupleToVec,t))
+    if type(t) is int: return str(t).strip('L')
+    if t and type(t[0]) is tuple:
+        return 'matrix [%s]' % ' '.join('[%s]' % ' '.join(map(str, x)) for x in t)
+    return 'vector [%s]' % ' '.join(map(tupleToVec,t))
 
 def vecToTuple(v):
-	v = v[1::-1]
-	if '[' in v:
-		return tuple(map(vecToTuple,(x.strip().strip('[') for x in v.split(']') if x.strip())))
-	return tuple(map(int, v.split()))
+    v = v[1:-1]
+    return tuple(map(int, v.split()))
 
 def send(ops):
-	return '\n'.join(v if type(v) is str else tupleToVec(v) for v in ops)
+    return '\n'.join(v if type(v) is str else tupleToVec(v) for v in ops)
 
 def recv(output):
-	out = map(str.strip, output.splitlines())[::-1]
-	res = []
-	while out:
-		x = out.pop()
-		if x[:2] == '[[':
-			x = x[1:]
-			m = []
-			while x != ']':
-				m.append(vecToTuple(x))
-				x = out.pop()
-			res.append(tuple(m))
-		else:
-			res.append(tupleToVec(x))
-	return tuple(res)
+    out = map(str.strip, output.splitlines())[::-1]
+    res = []
+    while out:
+        x = out.pop()
+        if x[:2] == '[[':
+            x = x[1:]
+            m = []
+            while x != ']':
+                m.append(vecToTuple(x))
+                x = out.pop()
+            res.append(tuple(m))
+        else:
+            res.append(vecToTuple(x))
+    return tuple(res)
 
 def evaluate(operations):
-	from subprocess import Popen, PIPE
+    from subprocess import Popen, PIPE
 
-	output, error = Popen(['vhe.exe'], stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True).communicate(send(operations))
-        
-	if error:
-		from sys import stderr
-		stderr.write(error + '\n')
-		stderr.flush()
+    inp = send(operations)
+    output, error = Popen(['./vhe'], stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True).communicate(inp)
+    
+    if error:
+        from sys import stderr
+        stderr.write(error + '\n')
+        stderr.flush()
 
-	return recv(output)
+    return recv(output)
 
 
 
